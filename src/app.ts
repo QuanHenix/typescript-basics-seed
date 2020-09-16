863e02dd1c65e33beba8160c50cef1fe59e9c088
QuanHenix <mqtran@henix.fr>
2020-09-16 15:35:20 +0200

    function type

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-let pizza: string | undefined | null = "pepperoni";
+let addSum = (price: number, number: number): number => {
+  return price * number;
+};
 
-function changePizzaName(newPizza: string): any {
-  pizza = null;
-}
+let addSum2: (price: number, number: number) => number;
+addSum2 = (x, y) => x + y;
 
-console.log(changePizzaName("mageritta"));
+console.log(addSum(20, 5));
+console.log(addSum2(20, 5));
