2e7862fa75ef235ef625298e8ad24611ce9995f7
QuanHenix <mqtran@henix.fr>
2020-09-16 15:00:35 +0200

    void type

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,7 @@
-//object destructuring
-const pizza = {
-  name: "pepperoni",
-  toppings: ["pepperoni"],
-};
+let pizza: string = "pepperoni";
 
-function order({ name: newName, toppings: newToppings }) {
-  return { newName, newToppings, price: 14 };
+function changePizzaName(newPizza: string): any {
+  pizza = newPizza;
 }
 
-// console.log(order(pizza).price);
-
-const { price } = order(pizza);
-// console.log(price);
-
-//array destructuring
-const topppings = ["pepperoni", "bacon", "chili"];
-
-const [a, b, c] = topppings;
-console.log(a, b, c);
-
-function logToppings([a, b, c]: any) {
-  console.log(a, b + c);
-}
-
-logToppings(topppings);
+console.log(changePizzaName("mageritta"));
