b5e7ea996b12cfeaab7b7e9879daa972f7d306f7
QuanHenix <mqtran@henix.fr>
2020-09-16 16:00:34 +0200

    function : optional argument and default parameter

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ let addSum = (price: number, number: number): number => {
   return price * number;
 };
 
-let addSum2: (price: number, number: number) => number;
-addSum2 = (x, y) => x + y;
+let addSum2: (price: number, number?: number) => number;
+addSum2 = (x, y = 1) => x + y;
 
 console.log(addSum(20, 5));
-console.log(addSum2(20, 5));
+console.log(addSum2(20));
