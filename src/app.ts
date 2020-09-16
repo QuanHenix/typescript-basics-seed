11b0b8118f07b304218defa2e08d1e45e5b209c3
QuanHenix <mqtran@henix.fr>
2020-09-16 10:58:00 +0200

    rest parameter

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-const pizzas = [{ name: "Pepperoni", toppings: ["pepperoni"] }];
+function addAll(message: string, ...arr: number[]) {
+  console.log(message);
+  return arr.reduce((prev, next) => prev + next);
+}
 
-const mappedPizza = pizzas.map(function (pizza) {
-  return pizza.name.toUpperCase();
-});
+const sum = addAll("Hello", 1, 2, 3, 4, 5, 6, 7, 8, 9);
 
-console.log(mappedPizza);
+console.log(sum);
